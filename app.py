f26a1abf9a3fcc512e4e9099139592a72683b3b4
Maksatai <erkbvna@gmail.com>
2021-07-14 13:05:31 +0600

    added encoding

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask,render_template,redirect
+from flask import Flask, render_template, redirect
 
 app=Flask(__name__)
 
 @app.route('/')
 def homepage():
-    f=open('goods.txt' ,'r' ,'utf-8')
+    f = open('goods.txt' ,'r',encoding='utf-8')
     txt=f.readlines()
     return render_template('index.html',goods=txt)
 
