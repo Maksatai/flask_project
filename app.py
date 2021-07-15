fa71dd00357114833b1e5389ab300bb8adf70487
Maksatai <erkbvna@gmail.com>
2021-07-15 17:13:56 +0600

    end

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 from flask.wrappers import Request
 import pandas as pd
+from openpyxl import Workbook
+from openpyxl import load_workbook
 
 app = Flask(__name__)
 
@@ -8,17 +10,18 @@ app = Flask(__name__)
 def homepage():
     excel_data_df = pd.read_excel(r'report.xlsx', engine='openpyxl')
 
-    return render_template('index.html',excel_f=excel_data_df.values, exc=excel_data_df)
+    return render_template('index.html',excel_f=excel_data_df.values, exc=excel_data_df,count=ws.max_row)
     
-
+outwb = load_workbook('report.xlsx')
+ws = outwb.active
 @app.route('/add/', methods=["POST"])
 def add():
     author = request.form["author"]
     descrip = request.form["descrip"]
     year = request.form["year"]
-    f = open('goods.txt','a+',encoding='utf-8')
-    f.write(author + '\n')
-    f.close()
+    ws.append([author,descrip,year])
+    outwb.save(filename='report.xlsx')
+    outwb.close()
     return """
         <h1>Архив добавлен</h1>
         <a href='/'>Домой</a>
